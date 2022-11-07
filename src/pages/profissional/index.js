951528171552cb9c07fe2172d89ac35903c212cc
RobertooFelipe <feliperoberto092@gmail.com>
2022-11-07 08:12:23 -0300

    feat: adicionando site no ambiente logado cli/pro

--- src/pages/profissional/index.js
+++ src/pages/profissional/index.js
@@ -28,27 +28,15 @@ function obtemDadosUsuario(uuid) {
         tipo: data[0].tipo,
       };
 
-      const htmlID = document.getElementById("datauser");
-      htmlID.innerHTML = ` <div class="cardInfoUser">
-                            <h3>Seja bem vindo ${userData.nome}</h3>
-                            <p>
-                              Você está acessando um novo ambiente, segue seu id e seu tipo
-                              de acesso: ${userData.uuid} | ${userData.tipo}
-                            </p>
-                            <br />
-                            <p>Seu usuario é: <strong>${userData.usuario}</strong></p>
-                            <br />
-                            <p>Seu email de acesso é: <strong>${userData.email}</strong></p>
-                            <br />
-                            <p>Sua senha de acesso é: <strong>${userData.senha}</strong></p>
-                          </div>`;
+      const htmlID = document.getElementById("boasVindas");
+      htmlID.innerHTML = `<p>Seja bem vindo, <strong>${userData.nome}</strong></p>`;
     })
     .catch((error) => {
       console.error("Error:", error);
     });
 }
 
-function voltar() {
+function sair() {
   sessionStorage.clear();
   window.location.replace(`../site/index.html`);
 }
