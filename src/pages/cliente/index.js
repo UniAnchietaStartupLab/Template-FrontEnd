e967004de7cb39031a5e91d269137183b9f8a744
RobertooFelipe <feliperoberto092@gmail.com>
2022-11-21 07:45:09 -0300

    feat: adicionando ambiente do perfil

--- src/pages/cliente/index.js
+++ src/pages/cliente/index.js
@@ -2,6 +2,7 @@ var params = window.location.search.substr(1).split("&");
 const uuid = atob(...params);
 const sessionSt = sessionStorage.getItem("uuid");
 obtemDadosUsuario(uuid);
+var userData;
 
 if (sessionSt === null) {
   alert("Sessão expirada!");
@@ -19,7 +20,7 @@ function obtemDadosUsuario(uuid) {
   })
     .then((response) => response.json())
     .then((data) => {
-      var userData = {
+      userData = {
         uuid: data[0].uuid,
         nome: data[0].nome,
         usuario: data[0].usuario,
@@ -51,12 +52,12 @@ function mostraConteudo(conteudo) {
       break;
     case "Profissionais":
       contentPage.innerHTML =
-        '<div id="contentProfissionais" class="contentProfissionais"> <h1>Profissionais</h1> <header> <input type="text" /><button> <i class="material-icons">search</i> </button> </header> <div class="contentList"> <button class="listaProfissionais"> <div> <h2>Roberto Felipe Nhani de Oliveira</h2> <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos sit, reiciendis accusamus atque itaque dolorum nulla pariatur, id quod praesentium quam obcaecati optio esse neque inventore. Ipsam omnis molestiae architecto. </p> <p>19 anos</p> </div> <div> <img src="../../assets/images/person.png" alt="profissional" /> </div> </button> <button class="listaProfissionais"> <div> <h2>Roberto Felipe Nhani de Oliveira</h2> <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos sit, reiciendis accusamus atque itaque dolorum nulla pariatur, id quod praesentium quam obcaecati optio esse neque inventore. Ipsam omnis molestiae architecto. </p> <p>19 anos</p> </div> <div> <img src="../../assets/images/person.png" alt="profissional" /> </div> </button> <button class="listaProfissionais"> <div> <h2>Roberto Felipe Nhani de Oliveira</h2> <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos sit, reiciendis accusamus atque itaque dolorum nulla pariatur, id quod praesentium quam obcaecati optio esse neque inventore. Ipsam omnis molestiae architecto. </p> <p>19 anos</p> </div> <div> <img src="../../assets/images/person.png" alt="profissional" /> </div> </button> <button class="listaProfissionais"> <div> <h2>Roberto Felipe Nhani de Oliveira</h2> <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos sit, reiciendis accusamus atque itaque dolorum nulla pariatur, id quod praesentium quam obcaecati optio esse neque inventore. Ipsam omnis molestiae architecto. </p> <p>19 anos</p> </div> <div> <img src="../../assets/images/person.png" alt="profissional" /> </div> </button> </div> </div>';
-      break;
-    case "Configuracoes":
-      contentPage.innerHTML = "";
+        '<div id="contentProfissionais" class="contentProfissionais"> <h1>Profissionais</h1> <header> <input type="text" /><button> <i class="material-icons">search</i> </button> </header> <div class="contentList"> <button class="listaProfissionais"> <div> <h2>Roberto Felipe Nhani de Oliveira</h2> <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos sit, reiciendis accusamus atque itaque dolorum nulla pariatur, id quod praesentium quam obcaecati optio esse neque inventore. Ipsam omnis molestiae architecto. </p> <p>19 anos</p> </div> <div> <img src="../../assets/images/person.png" alt="profissional" class="ftPerson"/> </div> </button> <button class="listaProfissionais"> <div> <h2>Roberto Felipe Nhani de Oliveira</h2> <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos sit, reiciendis accusamus atque itaque dolorum nulla pariatur, id quod praesentium quam obcaecati optio esse neque inventore. Ipsam omnis molestiae architecto. </p> <p>19 anos</p> </div> <div> <img src="../../assets/images/person.png" alt="profissional" class="ftPerson"/> </div> </button> <button class="listaProfissionais"> <div> <h2>Roberto Felipe Nhani de Oliveira</h2> <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos sit, reiciendis accusamus atque itaque dolorum nulla pariatur, id quod praesentium quam obcaecati optio esse neque inventore. Ipsam omnis molestiae architecto. </p> <p>19 anos</p> </div> <div> <img src="../../assets/images/person.png" alt="profissional" class="ftPerson"/> </div> </button> <button class="listaProfissionais"> <div> <h2>Roberto Felipe Nhani de Oliveira</h2> <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos sit, reiciendis accusamus atque itaque dolorum nulla pariatur, id quod praesentium quam obcaecati optio esse neque inventore. Ipsam omnis molestiae architecto. </p> <p>19 anos</p> </div> <div> <img src="../../assets/images/person.png" alt="profissional" class="ftPerson"/> </div> </button> </div> </div>';
       break;
     case "Perfil":
+      contentPage.innerHTML = `<div id="contentProfissionais" class="contentProfissionais"> <h1>Meu perfil</h1> <div> <div class="contentPersonData"> <div> <div class="optionPerson"> <p>Usuário:</p> <h3>${userData.usuario}</h3> </div> <div class="optionPerson"> <p>Nome:</p> <h3>${userData.nome}</h3> </div> <div class="optionPerson"> <p>Email:</p> <h3>${userData.email}</h3> </div> </div> <div> <img src="../../assets/images/person.png" alt="foto Perfil" class="ftPerson" /> </div> </div> <hr /> <div class="contentAdressData"> <div class="optionPerson"> <p>Endereço:</p> <h3>...</h3> </div> <div class="optionPerson"> <p>Número:</p> <h3>...</h3> </div> <div class="optionPerson"> <p>Bairro:</p> <h3>...</h3> </div> <div class="optionPerson"> <p>CEP:</p> <h3>...</h3> </div> <div class="optionPerson"> <p>Complemento:</p> <h3>...</h3> </div> </div> </div> </div>`;
+      break;
+    case "Configuracoes":
       contentPage.innerHTML = "";
       break;
   }
