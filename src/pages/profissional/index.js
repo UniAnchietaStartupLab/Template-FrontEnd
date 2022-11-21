2a45c522061e2ec5865333f58919271e4c66b9b1
RobertooFelipe <feliperoberto092@gmail.com>
2022-11-21 10:00:03 -0300

    fix: ambiente perfil

--- src/pages/profissional/index.js
+++ src/pages/profissional/index.js
@@ -2,6 +2,7 @@ var params = window.location.search.substr(1).split("&");
 const uuid = atob(...params);
 const sessionSt = sessionStorage.getItem("uuid");
 obtemDadosUsuario(uuid);
+var userData;
 
 if (sessionSt === null) {
   alert("Sessão expirada!");
@@ -19,7 +20,7 @@ function obtemDadosUsuario(uuid) {
   })
     .then((response) => response.json())
     .then((data) => {
-      var userData = {
+      userData = {
         uuid: data[0].uuid,
         nome: data[0].nome,
         usuario: data[0].usuario,
@@ -56,7 +57,7 @@ function mostraConteudo(conteudo) {
       contentPage.innerHTML = "";
       break;
     case "Perfil":
-      contentPage.innerHTML = "";
+      contentPage.innerHTML = `<div id="contentProfissionais" class="contentProfissionais"> <h1>Meu perfil</h1> <div> <div class="contentPersonData"> <div> <div class="optionPerson"> <p>Usuário:</p> <h3>${userData.usuario}</h3> </div> <div class="optionPerson"> <p>Nome:</p> <h3>${userData.nome}</h3> </div> <div class="optionPerson"> <p>Email:</p> <h3>${userData.email}</h3> </div> </div> <div> <img src="../../assets/images/person.png" alt="foto Perfil" class="ftPerson" /> </div> </div> <hr /> <div class="contentAdressData"> <h3>Dados Domiciliares</h3> <div class="optionPerson"> <p>Endereço:</p> <h3>...</h3> </div> <div class="optionPerson"> <p>Número:</p> <h3>...</h3> </div> <div class="optionPerson"> <p>Bairro:</p> <h3>...</h3> </div> <div class="optionPerson"> <p>CEP:</p> <h3>...</h3> </div> <div class="optionPerson"> <p>Complemento:</p> <h3>...</h3> </div> </div> <hr /> <div class="aboutProfessional"> <h3>Dados profissionais</h3> <div class="optionPerson"> <p>Profissão:</p> <h3>...</h3> </div> <div class="optionPerson"> <p>Sobre mim:</p> <h3>...</h3> </div> <div class="optionPerson"> <p>Formação:</p> <h3>...</h3> </div> <div class="optionPerson"> <p>Referências:</p> <h3>...</h3> </div> <div class="optionPerson"> <p>Contato:</p> <h3>...</h3> </div> </div> </div> </div>`;
       break;
   }
 }
